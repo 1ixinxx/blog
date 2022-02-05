04489f07732ec74dc3599d7196ce88b74b63509c
1ixinxx <97590667+1ixinxx@users.noreply.github.com>
2022-02-05 12:38:31 +0800

    Create lixinblog.js

---
+++ lixinblog.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d88888ab7e025bede64ee18a28031e52";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
