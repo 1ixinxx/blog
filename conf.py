30753ca6a607fce0a283d9d380bc00f40df6b0b8
1ixinxx <97590667+1ixinxx@users.noreply.github.com>
2022-02-05 12:39:57 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -60,3 +60,7 @@ html_theme = 'classic'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+#流量统计
+html_js_files = [
+    'js/lixinblog.js'
+]
